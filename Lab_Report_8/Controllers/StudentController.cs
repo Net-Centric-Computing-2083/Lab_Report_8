982327f7533527ef11965230d9a2ab0be07b6665
Amrita631 <amritacsit23@oic.edu.np>
2026-09-05 22:10:37 +0545

    Lab_Report_8 Rendering HTML Content Using Views in ASP.NET Core MVC

---
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_Report_8.Models;
+
+namespace Lab_Report_8.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Details()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Amrita Singh",
+                Department = "Computer Engineering",
+                Cgpa = 3.75
+            };
+
+            return View(student);
+        }
+    }
+}
---
+++ Lab_Report_8/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_Report_8.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public double Cgpa { get; set; }
+    }
+}
