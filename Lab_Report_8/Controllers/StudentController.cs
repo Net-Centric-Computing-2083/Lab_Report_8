eb41318e039d2b597a2b64ad1611967f90e4219e
Rojesh Niroula <wonderingmind2005@gmail.com>
2026-09-05 08:04:34 +0545

    This is my lab 8

---
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using LabReport8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabReport8.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /Student/Details
+        public IActionResult Details()
+        {
+            // In a real application this data would come from a database.
+            Student student = new Student
+            {
+                Id = 101,
+                Name = "Rojesh Niroula",
+                Course = "Net-Centric Computing",
+                Semester = 6,
+                Email = "rojesh@example.com"
+            };
+
+            return View(student);
+        }
+    }
+}
---
+++ Lab_Report_8/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabReport8.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Course { get; set; }
+
+        public int Semester { get; set; }
+
+        public string Email { get; set; }
+    }
+}
