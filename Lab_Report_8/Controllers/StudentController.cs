34dcd6a23acbbaf59e030da82b382b87db7bcf0d
aakritidhungana <aakritidhungana123@gmail.com>
2026-09-06 21:32:29 +0545

    lab 8 commit

---
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWelcomeApp.Models;
+
+namespace MVCWelcomeApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Details()
+        {
+            // Creating student object
+            Student student = new Student
+            {
+                Id = 911,
+                Name = "Aakriti",
+                Faculty = "BSc. CSIT",
+                Semester = 6,
+                Address = "Kathmandu"
+            };
+
+            // Passing student data to the View
+            return View(student);
+        }
+    }
+}
