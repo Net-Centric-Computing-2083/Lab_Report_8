f28652b32ee87ea36bd3dd65e3574b0fb26a6236
Aadarsha <aadarshakhadka710@gmail.com>
2026-09-07 06:43:42 +0545

    Lab 8: Complete

---
+++ Lab_Report_8/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_Report_8.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public string Email { get; set; }
+    }
+}
