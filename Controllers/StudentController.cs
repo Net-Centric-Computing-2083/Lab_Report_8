69ca1e92ab618ce03c7267aead61e7a3ff9b4ed7
Aadarsha <aadarsha23@oic.edu.np>
2026-09-07 10:37:14 +0545

    Initial Commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_Report_8.Models;
+
+namespace Lab_Report_8.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Details()
+        {
+            Student student = new Student
+            {
+                Id = 1,
+                Name = "Aadarsha Gautam",
+                Faculty = "BSc CSIT",
+                Email = "aadarsha@oic.edu.np"
+            };
+
+            return View(student);
+        }
+    }
+}
