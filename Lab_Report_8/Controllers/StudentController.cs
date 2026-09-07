26c17d327db7aef46a264caba891273e846805a4
Acer <Acer@SUSHANT>
2026-09-07 07:39:58 +0545

    LAB 8 COMMITTED

---
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_Report_8.Models;
+
+namespace Lab_Report_8.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Details()
+        {
+            Student student = new Student
+            {
+                Id = 80010955,
+                Name = "Sushant Pokhrel",
+                Faculty = "BSc CSIT",
+                Email = "sushantcsit23@oic.edu.np"
+            };
+
+            return View(student);
+        }
+    }
+}
