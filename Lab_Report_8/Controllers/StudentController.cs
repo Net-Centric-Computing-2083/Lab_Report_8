d15515493a6d376785f735d2540ba16fa6c10651
gayatri <gayatrijoshi2063@gmail.com>
2026-09-09 08:10:10 +0545

    lab report 8

---
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCViewsDemo.Models;
+
+namespace MVCViewsDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Details()
+        {
+            Student student = new Student
+            {
+                Id = 928,
+                Name = "Gayatri Joshi",
+                Course = "Bsc.CSIT",
+                Age = 21,
+                Semester = 6
+            };
+
+            return View(student);       
+        }
+    }
+}
---
+++ Lab_Report_8/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCViewsDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Course { get; set; }
+        public int Age { get; set; }
+        public int ?Semester { get; set; }
+    }
+}
