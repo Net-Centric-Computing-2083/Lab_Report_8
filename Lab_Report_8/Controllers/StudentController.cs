1bd977b2914d469cbe614f962e80bfaca7217209
rusha13 <rushapokharel13@gmail.com>
2026-09-09 09:13:23 +0545

    Final program.

--- Lab_Report_8/Controllers/StudentController.cs
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Lab_Report_8.Models;
+﻿using Lab8.Models;
+using Lab8.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Lab_Report_8.Controllers
+namespace LabReport8.Controllers
 {
     public class StudentController : Controller
     {
@@ -10,9 +11,8 @@ namespace Lab_Report_8.Controllers
             Student student = new Student
             {
                 Id = 1,
-                Name = "Amrita Singh",
-                Department = "Computer Engineering",
-                Cgpa = 3.75
+                Name = "Rusha Pokharel",
+                Course = "CSIT"
             };
 
             return View(student);
--- Lab_Report_8/Models/Student.cs
+++ Lab_Report_8/Models/Student.cs
@@ -1,10 +1,11 @@
-﻿namespace Lab_Report_8.Models
+﻿namespace Lab8.Models
 {
     public class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Department { get; set; } = string.Empty;
-        public double Cgpa { get; set; }
+
+        public string Name { get; set; }
+
+        public string Course { get; set; }
     }
 }
