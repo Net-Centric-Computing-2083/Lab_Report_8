4ee383da67d87ff2662301baa59bdede8cba18ea
Diwakar Lamichhane <153197677+Diwakar-lamichhane77@users.noreply.github.com>
2026-09-15 19:05:16 +0545

    Refactor StudentController and update student details

--- Lab_Report_8/Controllers/StudentController.cs
+++ Lab_Report_8/Controllers/StudentController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Lab_Report_8.Models;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
 
-namespace Lab_Report_8.Controllers
+namespace MVCDemo.Controllers
 {
     public class StudentController : Controller
     {
@@ -10,9 +10,10 @@ namespace Lab_Report_8.Controllers
             Student student = new Student
             {
                 Id = 1,
-                Name = "Amrita Singh",
-                Department = "Computer Engineering",
-                Cgpa = 3.75
+                Name = "Diwakar Lamichhane",
+                Age = 23,
+                Address = "Kathmandu, Nepal",
+                Semester = 6
             };
 
             return View(student);
